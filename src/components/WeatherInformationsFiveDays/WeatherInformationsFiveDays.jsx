e7fa2eb4cab867c1626732e010d075c08bb88c85
Gabriel Fiorim Esteves <123314503+Fiorim1@users.noreply.github.com>
2025-09-21 20:26:28 -0300

    Update WeatherInformationsFiveDays.jsx

--- src/components/WeatherInformationsFiveDays/WeatherInformationsFiveDays.jsx
+++ src/components/WeatherInformationsFiveDays/WeatherInformationsFiveDays.jsx
@@ -2,6 +2,23 @@ import './WeatherInformationsFiveDays.css'
 
 function WeatherInformations({ weatherFiveDays }) {
     console.log(weatherFiveDays)
+
+    const dailyForecast = {
+
+    }
+
+    for(let forecast of weatherFiveDays.list) {
+        const date = new Date(forecast.dt *1000).toLocaleDateString()
+
+        if(!dailyForecast[date]){
+            dailyForecast[date] = forecast
+        }
+    }
+
+    const nextFiveDays = Object.values(dailyForecast).slice(1,6)
+    
+    const nextFiveDaysForeCast = Object.values(dailyForecast) 
+
     return (
         <div className="weather-container">
             <p>5 Days</p>
